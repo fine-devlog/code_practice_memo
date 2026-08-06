0dc348d55964a655ff5c06e1bd9bda2857a5a803
fine <misaki.ywkt@icloud.com>
2026-08-06 11:23:52 +0900

    Add files via upload

---
+++ python-course/02_01/02_01.py
@@ -0,0 +1,10 @@
+dic={'apple':200,'orenge':150,'cherry':350,'pear':250}
+print(dic)
+print(f"orengeの値段は{dic['orenge']}円です。")
+'banana'in dic
+dic['apple']=230
+print(dic)
+num=2
+price=dic['apple']
+price=(price)*(num)
+print(f"apple{num}個の値段は{price}円です。")
